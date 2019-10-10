0de3a34606921788e38145e5074a4f1f34b82173
chenxiangpeng <xiangpeng_chen@126.com>
2019-10-10 14:30:45 +0800

    filelist mode added

--- lua-apps/apps/push_file.main.lua
+++ lua-apps/apps/push_file.main.lua
@@ -14,20 +14,24 @@ local kPeerName = "--peer-name";
 local kAsServer = "--as-server";
 local kServer = "--server";
 local kPort = "--port";
-local kLocalFile="--local";
-local kRemoteFile="--remote";
+local kLocalFile = "--local";
+local kRemoteFile = "--remote";
+local kIsFileList = "--is-file-list"
+local kStrip = "--strip";
+local kPathPrefix = "--path-prefix";
 
 function app_main(args)
     local cmd = CommandLine.new();
 
-    cmd:AddKeyType(kPeerName,TYPE_KEY_EQUAL_VALUE,OPTIONAL,"peer name of the server,default: simple-file-client");
-    
+    cmd:AddKeyType(kPeerName,TYPE_KEY_EQUAL_VALUE,OPTIONAL,"peer name of the server,default: simple-file-client");    
     cmd:AddKeyType(kAsServer,TYPE_KEY,OPTIONAL,"as tcp server");
     cmd:AddKeyType(kServer,TYPE_KEY_EQUAL_VALUE,OPTIONAL,"message peer server address,default: 127.0.0.1");
     cmd:AddKeyType(kPort,TYPE_KEY_EQUAL_VALUE,OPTIONAL,"message peer port, default: 2016")
-
     cmd:AddKeyType(kLocalFile,TYPE_KEY_EQUAL_VALUE,MUST,"local file to push");
     cmd:AddKeyType(kRemoteFile,TYPE_KEY_EQUAL_VALUE,MUST,"remote file to save");
+    cmd:AddKeyType(kIsFileList,TYPE_KEY,OPTIONAL,"local file is a file list actually.");
+    cmd:AddKeyType(kStrip,TYPE_KEY_EQUAL_VALUE,OPTIONAL,"path strip when use file list mode.");
+    cmd:AddKeyType(kPathPrefix,TYPE_KEY_EQUAL_VALUE,OPTIONAL,"path prefix when use file list mode.");
     cmd:LoadFromArgv(args);            
     
     if cmd:CheckForErrors() then
@@ -57,6 +61,17 @@ function app_main(args)
         printfnl("start message center on port %d",port);
     end
 
+    local path_strip,path_prefix;
+    if cmd:HasKey(kStrip) then
+        path_strip = cmd:GetValueByKey(kStrip);
+    end
+
+    if cmd:HasKey(kPathPrefix) then
+        path_prefix = cmd:GetValueByKey(kPathPrefix);
+    end
+
+    local is_file_list = cmd:HasKey(kIsFileList);
+
     local local_file =FileManager.ToAbsPath(cmd:GetValueByKey(kLocalFile));
     local remote_file = cmd:GetValueByKey(kRemoteFile);
     
@@ -80,7 +95,28 @@ function app_main(args)
         file_client:SetDestPeerName("simple-file-server");
         file_client:Start();
 
-        if not local_dir then
+        if is_file_list then
+            for_each_line(local_file,function(line)                
+                local rpath = line:CStr();
+
+                if path_strip then
+                    rpath = remove_path_prefix(rpath,path_strip);
+                end
+
+                local remote_path = remote_file.."/"..rpath;
+
+                if path_prefix then
+                    rpath = path_prefix.."/"..rpath;
+                end
+
+                local local_path=FileManager.ToAbsPath(rpath);                
+                if not file_client:PushBigFile(local_path,remote_path) then
+                    printfnl("push file fail %s",local_path);
+                else
+                    printfnl("send %s ok",local_path);
+                end    
+            end);
+        elseif not local_dir then
             if not file_client:PushBigFile(local_file,remote_file) then
                 printfnl("push file fail %s",local_file);
             else
@@ -101,7 +137,7 @@ function app_main(args)
                 else
                     printfnl("send %s ok",v);                    
                 end
-            end
+            end       
         end
 
         while file_client:GetSendingQueueLength() > 0 do            
