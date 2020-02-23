4c6e6c4f8f3460845b7cfa2cd6cbb3f31eb4e44b
chenxiangpeng <xiangpeng_chen@126.com>
2020-02-23 19:18:52 +0800

    update build time

--- _build_time_.h
+++ _build_time_.h
@@ -1,8 +1,8 @@
 #ifndef __BUILD_TIME__H
 #define __BUILD_TIME__H
 
-#define BUILD_DATE "2020-02-08"
-#define BUILD_TIME "16:43:12"
+#define BUILD_DATE "2020-02-23"
+#define BUILD_TIME "19:17:43"
 
 #endif
 
