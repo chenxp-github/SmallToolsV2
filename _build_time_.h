b7af9084f31c9a0eacb4928c81c01080c9c7d5e3
chenxp <xiangpeng_chen@126.com>
2020-09-29 12:49:38 +0800

    merge code

--- _build_time_.h
+++ _build_time_.h
@@ -1,8 +1,8 @@
 #ifndef __BUILD_TIME__H
 #define __BUILD_TIME__H
 
-#define BUILD_DATE "2020-05-26"
-#define BUILD_TIME "17:14:52"
+#define BUILD_DATE "2020-09-29"
+#define BUILD_TIME "12:41:23"
 
 #endif
 
