b455acc4539806666d3592ce350e2c618d4f9e27
chenxp <xiangpeng_chen@126.com>
2020-10-18 10:38:15 +0800

    update xml

--- _build_time_.h
+++ _build_time_.h
@@ -1,8 +1,8 @@
 #ifndef __BUILD_TIME__H
 #define __BUILD_TIME__H
 
-#define BUILD_DATE "2020-09-29"
-#define BUILD_TIME "12:41:23"
+#define BUILD_DATE "2020-10-18"
+#define BUILD_TIME "10:37:49"
 
 #endif
 
