f4d7e19e2d586a9eb93b64d14662ed5b1ac535fa
chenxp <xiangpeng_chen@126.com>
2021-04-23 11:28:39 +0800

    support multi tcp tunnel port

--- lua-apps/apps/tcp_tunnel.main.lua
+++ lua-apps/apps/tcp_tunnel.main.lua
@@ -21,6 +21,7 @@ local kRemoteServer="--remote-server";
 local kRemotePort="--remote-port";
 local kLocalListeningPort="--local-listening-port";
 local kTimeout="--timeout";
+local kConfig="--config";
 
 function app_main(args)
     local argc = #args;
@@ -31,7 +32,10 @@ function app_main(args)
     cmd:AddKeyType(kPeerServer,TYPE_KEY_EQUAL_VALUE,MUST,"message peer server address");
     cmd:AddKeyType(kPeerPort,TYPE_KEY_EQUAL_VALUE,MUST,"message center service port");    
     cmd:AddKeyType(kPeerName,TYPE_KEY_EQUAL_VALUE,MUST,"self message peer name");        
+    cmd:AddKeyType(kRemotePeerName,TYPE_KEY_EQUAL_VALUE,MUST,"remote message peer name");    
     cmd:AddKeyType(kTimeout,TYPE_KEY_EQUAL_VALUE,OPTIONAL,"socket inactive timeout (ms).");
+    cmd:AddKeyType(kConfig,TYPE_KEY_EQUAL_VALUE,OPTIONAL,"config file as client");
+
     cmd:LoadFromArgv(args);  
     if cmd:CheckForErrors() then
         return 1;
@@ -44,36 +48,74 @@ function app_main(args)
         exit("you must specify at least one %s or %s",kAsServer,kAsServer);
     end
 
+    local remote_peer_name = cmd:GetValueByKey(kRemotePeerName);
     local peer_server = cmd:GetValueByKey(kPeerServer);
     local peer_port = tonumber(cmd:GetValueByKey(kPeerPort));
     local peer_name = cmd:GetValueByKey(kPeerName);    
-    
+    local client_table={};
+
     if is_client then
-        local cmd = CommandLine.new();
-        cmd:AddKeyType(kRemoteServer,TYPE_KEY_EQUAL_VALUE,MUST,"remote server address connect to");
-        cmd:AddKeyType(kRemotePort,TYPE_KEY_EQUAL_VALUE,MUST,"remote port connect to");
-        cmd:AddKeyType(kLocalListeningPort,TYPE_KEY_EQUAL_VALUE,MUST,"local listening port");
-        cmd:AddKeyType(kRemotePeerName,TYPE_KEY_EQUAL_VALUE,MUST,"remote message peer name");
+        if cmd:HasKey(kConfig) then
+            local cmd = CommandLine.new();
+            cmd:AddKeyType(kConfig,TYPE_KEY_EQUAL_VALUE,MUST,"config file as client");
 
-        cmd:LoadFromArgv(args);
-        if cmd:CheckForErrors() then
-            return 1;
+            cmd:LoadFromArgv(args);
+            if cmd:CheckForErrors() then
+                return 1;
+            end
+
+            local config = cmd:GetValueByKey(kConfig);
+            if not exec_file(config) then
+                return exit("execute %s fail.",config);
+            end
+
+            if not tunnel_table then
+                return exit("can not find 'tunnel_table' in config file");
+            end
+
+            for i,v in ipairs(tunnel_table) do
+                local remote_server = v[1];
+                local remote_port = v[2];
+                local local_listening_port = v[3];
+
+                local client = PeerTunnelClient.new();
+                client:InitClientSidePeer(peer_server,peer_port);
+                client:SetName(peer_name.."-"..i);
+                client:SetDestPeerName(remote_peer_name);
+                client:SetRemote(remote_server,remote_port);
+                client:Start();        
+                client:StartListeningLocalPort(local_listening_port);
+                client:StartAutoClearThread();
+                printfnl("listening on local port %d -> %s:%d",
+                    local_listening_port,remote_server,remote_port);
+                table.insert(client_table,client);
+            end
+        else
+            local cmd = CommandLine.new();
+            cmd:AddKeyType(kRemoteServer,TYPE_KEY_EQUAL_VALUE,MUST,"remote server address connect to");
+            cmd:AddKeyType(kRemotePort,TYPE_KEY_EQUAL_VALUE,MUST,"remote port connect to");
+            cmd:AddKeyType(kLocalListeningPort,TYPE_KEY_EQUAL_VALUE,MUST,"local listening port");            
+            cmd:AddKeyType(kConfig,TYPE_KEY_EQUAL_VALUE,MUST,"config file as client");
+
+            cmd:LoadFromArgv(args);
+            if cmd:CheckForErrors() then
+                return 1;
+            end    
+
+            local remote_server = cmd:GetValueByKey(kRemoteServer);
+            local remote_port = tonumber(cmd:GetValueByKey(kRemotePort));
+            local local_listening_port = tonumber(cmd:GetValueByKey(kLocalListeningPort));
+    
+            local client = PeerTunnelClient.new();
+            client:InitClientSidePeer(peer_server,peer_port);
+            client:SetName(peer_name);
+            client:SetDestPeerName(remote_peer_name);
+            client:SetRemote(remote_server,remote_port);
+            client:Start();        
+            client:StartListeningLocalPort(local_listening_port);
+            client:StartAutoClearThread();
+            printfnl("listening on local port %d",local_listening_port);
         end
-
-        local remote_peer_name = cmd:GetValueByKey(kRemotePeerName);
-        local remote_server = cmd:GetValueByKey(kRemoteServer);
-        local remote_port = tonumber(cmd:GetValueByKey(kRemotePort));
-        local local_listening_port = tonumber(cmd:GetValueByKey(kLocalListeningPort));
-
-        local client = PeerTunnelClient.new();
-        client:InitClientSidePeer(peer_server,peer_port);
-        client:SetName(peer_name);
-        client:SetDestPeerName(remote_peer_name);
-        client:SetRemote(remote_server,remote_port);
-        client:Start();        
-        client:StartListeningLocalPort(local_listening_port);
-        client:StartAutoClearThread();
-        printfnl("listening on local port %d",local_listening_port);
     elseif is_server then
         local server = PeerTunnelServer.new();
         server:InitClientSidePeer(peer_server,peer_port);
