4ebd38d430981d04d3a99e352549b4ea3e5df2dc
wanggeng <wanggeng07@qq.com>
2023-07-29 22:13:25 +0800

    covert mouse xy

--- remote-desktop/remote_desktop_server.cpp
+++ remote-desktop/remote_desktop_server.cpp
@@ -20,8 +20,8 @@ CRemoteDesktopServer::~CRemoteDesktopServer()
 status_t CRemoteDesktopServer::InitBasic()
 {
     CRpcServiceBase::InitBasic();
-	m_SessionManager.InitBasic();
-	m_SnapshottorManager = NULL;
+    m_SessionManager.InitBasic();
+    m_SnapshottorManager = NULL;
     return OK;
 }
 
@@ -29,14 +29,14 @@ status_t CRemoteDesktopServer::Init(CTaskMgr *mgr)
 {
     this->InitBasic();
     CRpcServiceBase::Init(mgr);
-	m_SessionManager.Init(mgr);
-	m_SessionManager.StartAutoClearTask();
+    m_SessionManager.Init(mgr);
+    m_SessionManager.StartAutoClearTask();
     return OK;
 }
 
 status_t CRemoteDesktopServer::Destroy()
 {
-	m_SessionManager.Destroy();
+    m_SessionManager.Destroy();
     CRpcServiceBase::Destroy();
     this->InitBasic();
     return OK;
@@ -59,7 +59,7 @@ status_t CRemoteDesktopServer::OnRequest(CPeerMessage *_message)
     int method = _message->GetFunc();
     this->SetDestPeerName(_message->GetFrom());
 
-	m_SessionManager.SessionReactive(_message->GetFrom());
+    m_SessionManager.SessionReactive(_message->GetFrom());
 
 /*##Begin OnRequest_Cpp ##*/
     switch(method)
@@ -101,29 +101,29 @@ status_t CRemoteDesktopServer::OnLogin(CPeerMessage *_message)
     
     CRetVal_RdLogin _ret;
     _ret.Init();
-	
-	if(_param.GetVersion() != REMOTE_DESKTOP_VERSION)
-	{
-		_ret.SetRetVal0(-1);
-		return this->SendReturnValue(&_context,&_ret);
-	}
+    
+    if(_param.GetVersion() != REMOTE_DESKTOP_VERSION)
+    {
+        _ret.SetRetVal0(-1);
+        return this->SendReturnValue(&_context,&_ret);
+    }
 
-	CMem *sessionName = _message->GetFrom();
-	ASSERT(sessionName);
+    CMem *sessionName = _message->GetFrom();
+    ASSERT(sessionName);
 
-	if(m_SessionManager.GetSession(sessionName))
-	{
-		m_SessionManager.DelSession(sessionName);
-	}
+    if(m_SessionManager.GetSession(sessionName))
+    {
+        m_SessionManager.DelSession(sessionName);
+    }
 
-	if(m_SessionManager.CreateSession(sessionName))
-	{
-		_ret.SetRetVal0(OK);
-	}
-	else
-	{
-		_ret.SetRetVal0(ERROR);
-	}
+    if(m_SessionManager.CreateSession(sessionName))
+    {
+        _ret.SetRetVal0(OK);
+    }
+    else
+    {
+        _ret.SetRetVal0(ERROR);
+    }
     this->SendReturnValue(&_context,&_ret);
     return OK;
 }
@@ -142,17 +142,17 @@ status_t CRemoteDesktopServer::OnGetMonitorList(CPeerMessage *_message)
     CRetVal_RdGetMonitorList _ret;
     _ret.Init();
 
-	ASSERT(m_SnapshottorManager);
+    ASSERT(m_SnapshottorManager);
 
-	CMemStk list;
-	list.Init();
-	m_SnapshottorManager->GetNameList(&list);
+    CMemStk list;
+    list.Init();
+    m_SnapshottorManager->GetNameList(&list);
 
-	_ret.AllocMonitors(list.GetLen());
-	for(int i = 0; i < list.GetLen(); i++)
-	{
-		_ret.SetMonitorsElem(i,list.GetElem(i));
-	}
+    _ret.AllocMonitors(list.GetLen());
+    for(int i = 0; i < list.GetLen(); i++)
+    {
+        _ret.SetMonitorsElem(i,list.GetElem(i));
+    }
     
     this->SendReturnValue(&_context,&_ret);
     return OK;
@@ -175,62 +175,62 @@ status_t CRemoteDesktopServer::OnGetNextScreen(CPeerMessage *_message)
     CRetVal_RdGetNextScreen _ret;
     _ret.Init();
 
-	CRemoteDesktopSession *session = m_SessionManager.GetSession(_message->GetFrom());
-	if(!session)
-	{
-		_ret.SetRes(-2);
-		this->SendReturnValue(&_context,&_ret);
-		return OK;
-	}
+    CRemoteDesktopSession *session = m_SessionManager.GetSession(_message->GetFrom());
+    if(!session)
+    {
+        _ret.SetRes(-2);
+        this->SendReturnValue(&_context,&_ret);
+        return OK;
+    }
 
-	ASSERT(m_SnapshottorManager);
-	
-	CRemoteDesktopPixelBuffer pbuf;
-	pbuf.Init();
+    ASSERT(m_SnapshottorManager);
+    
+    CRemoteDesktopPixelBuffer pbuf;
+    pbuf.Init();
 
-	if(!m_SnapshottorManager->TakeScreenSnapshot(_param.GetMonitorName(),&pbuf))
-	{
-		_ret.SetRes(-3);
-		this->SendReturnValue(&_context,&_ret);
-		return OK;
-	}
-	
-	if(!pbuf.ChangeBpp(_param.GetBytePerPixel()))
-	{
-		_ret.SetRes(-4);
-		this->SendReturnValue(&_context,&_ret);
-		return OK;
-	}
+    if(!m_SnapshottorManager->TakeScreenSnapshot(_param.GetMonitorName(),&pbuf))
+    {
+        _ret.SetRes(-3);
+        this->SendReturnValue(&_context,&_ret);
+        return OK;
+    }
+    
+    if(!pbuf.ChangeBpp(_param.GetBytePerPixel()))
+    {
+        _ret.SetRes(-4);
+        this->SendReturnValue(&_context,&_ret);
+        return OK;
+    }
 
-	CRemoteDesktopPixelBufferList *diff = _ret.GetDiff();
-	ASSERT(diff);
+    CRemoteDesktopPixelBufferList *diff = _ret.GetDiff();
+    ASSERT(diff);
 
-	CRemoteDesktopPixelBuffer *cur_screen = session->GetCurScreen();
-	ASSERT(cur_screen);
+    CRemoteDesktopPixelBuffer *cur_screen = session->GetCurScreen();
+    ASSERT(cur_screen);
 
-	if(_param.GetFull() || pbuf.IsSizeOrBppChanged(cur_screen) || session->GetFrameNo()!=_param.GetCurFrameNo())
-	{
-		cur_screen->Transfer(&pbuf);		
-		session->SetFrameNo(_param.GetCurFrameNo()+1);
-		diff->CopySingleScreen(cur_screen);
-		diff->CompressAll();
-		_ret.SetFull(1);
-		_ret.SetFrameNo(session->GetFrameNo());
-		//XLOG(LOG_MODULE_USER,LOG_LEVEL_ERROR,"full size=%d",diff->GetTotalPixelDataSize());
-	}
-	else
-	{	
-		cur_screen->ChangeBpp(pbuf.GetBytePerPixel());
-		cur_screen->Diff(&pbuf,64,64,diff);
-		cur_screen->Transfer(&pbuf);	
-		diff->CompressAll();
-		_ret.SetFull(0);
-		session->SetFrameNo(_param.GetCurFrameNo()+1);
-		_ret.SetFrameNo(session->GetFrameNo());
-		//XLOG(LOG_MODULE_USER,LOG_LEVEL_ERROR,"part(%d) size=%d",diff->GetLen(),diff->GetTotalPixelDataSize());
-	}
+    if(_param.GetFull() || pbuf.IsSizeOrBppChanged(cur_screen) || session->GetFrameNo()!=_param.GetCurFrameNo())
+    {
+        cur_screen->Transfer(&pbuf);		
+        session->SetFrameNo(_param.GetCurFrameNo()+1);
+        diff->CopySingleScreen(cur_screen);
+        diff->CompressAll();
+        _ret.SetFull(1);
+        _ret.SetFrameNo(session->GetFrameNo());
+        //XLOG(LOG_MODULE_USER,LOG_LEVEL_ERROR,"full size=%d",diff->GetTotalPixelDataSize());
+    }
+    else
+    {	
+        cur_screen->ChangeBpp(pbuf.GetBytePerPixel());
+        cur_screen->Diff(&pbuf,64,64,diff);
+        cur_screen->Transfer(&pbuf);	
+        diff->CompressAll();
+        _ret.SetFull(0);
+        session->SetFrameNo(_param.GetCurFrameNo()+1);
+        _ret.SetFrameNo(session->GetFrameNo());
+        //XLOG(LOG_MODULE_USER,LOG_LEVEL_ERROR,"part(%d) size=%d",diff->GetLen(),diff->GetTotalPixelDataSize());
+    }
 
-	_ret.SetRes(OK);
+    _ret.SetRes(OK);
     this->SendReturnValue(&_context,&_ret);
     return OK;
 }
@@ -240,38 +240,38 @@ status_t CRemoteDesktopServer::OnGetNextScreen(CPeerMessage *_message)
 #if _IS_WINDOWS_
 static status_t SendLocaInput(CRemoteDesktopServer *self,CRemoteDesktopInput *input)
 {
-	ASSERT(input);
+    ASSERT(input);
 
-	INPUT localIput;
-	memset(&localIput,0,sizeof(localIput));
+    INPUT localIput;
+    memset(&localIput,0,sizeof(localIput));
 
-	localIput.type = input->GetType();
+    localIput.type = input->GetType();
 
-	if(input->GetType() == RD_INPUT_TYPE_KEYBOARD_INPUT)
-	{
-		CRemoteDesktopKeybdInput *ki = input->GetKeybdInput();
-		ASSERT(ki);
-		localIput.ki.dwFlags = ki->GetDwFlags();
-		localIput.ki.wScan = ki->GetWScan();
-		localIput.ki.wVk = ki->GetWVk();
-	}
-	else if(input->GetType() == RD_INPUT_TYPE_MOUSE_INPUT)
-	{
-		CRemoteDesktopMouseInput *mi = input->GetMouseInput();
-		ASSERT(mi);
-		localIput.mi.dwFlags = mi->GetDwFlags();
-		localIput.mi.dx = mi->GetDx();
-		localIput.mi.dy = mi->GetDy();
-		localIput.mi.mouseData = mi->GetMouseData();
-	}
-	else
-	{
-		XLOG(LOG_MODULE_USER,LOG_LEVEL_ERROR,"not supported input type %u",input->GetType());
-		return ERROR;
-	}
+    if(input->GetType() == RD_INPUT_TYPE_KEYBOARD_INPUT)
+    {
+        CRemoteDesktopKeybdInput *ki = input->GetKeybdInput();
+        ASSERT(ki);
+        localIput.ki.dwFlags = ki->GetDwFlags();
+        localIput.ki.wScan = ki->GetWScan();
+        localIput.ki.wVk = ki->GetWVk();
+    }
+    else if(input->GetType() == RD_INPUT_TYPE_MOUSE_INPUT)
+    {
+        CRemoteDesktopMouseInput *mi = input->GetMouseInput();
+        ASSERT(mi);
+        localIput.mi.dwFlags = mi->GetDwFlags();
+        localIput.mi.dx = mi->GetDx();
+        localIput.mi.dy = mi->GetDy();
+        localIput.mi.mouseData = mi->GetMouseData();
+    }
+    else
+    {
+        XLOG(LOG_MODULE_USER,LOG_LEVEL_ERROR,"not supported input type %u",input->GetType());
+        return ERROR;
+    }
 
-	::SendInput(1,&localIput,sizeof(localIput));
-	return OK;
+    ::SendInput(1,&localIput,sizeof(localIput));
+    return OK;
 }
 
 #else
@@ -282,69 +282,73 @@ static status_t SendLocaInput(CRemoteDesktopServer *self,CRemoteDesktopInput *in
 #include "remote_desktop_snapshottor_x11.h"
 static status_t SendLocaInput(CRemoteDesktopServer *self,CRemoteDesktopInput *input)
 {	
-	ASSERT(input);
-	ASSERT(self);
-	ASSERT(self->m_SnapshottorManager);
+    ASSERT(input);
+    ASSERT(self);
+    ASSERT(self->m_SnapshottorManager);
 
-	CMem *monitorName = input->GetMonitorName();
-	ASSERT(monitorName);
+    CMem *monitorName = input->GetMonitorName();
+    ASSERT(monitorName);
 
-	CRemoteDesktopSnapshottorManager *snapshotManager = self->m_SnapshottorManager;
+    CRemoteDesktopSnapshottorManager *snapshotManager = self->m_SnapshottorManager;
 
-	CRemoteDesktopSnapshottor *snapshottor = snapshotManager->GetByName(monitorName);
-	ASSERT(snapshottor);
-	ASSERT(snapshottor->GetType() == CRemoteDesktopSnapshottor::SNAPSHOTTOR_TYPE_X11);
-	CRemoteDesktopSnapshottor_X11 *x11_ss = (CRemoteDesktopSnapshottor_X11*)snapshottor;
+    CRemoteDesktopSnapshottor *snapshottor = snapshotManager->GetByName(monitorName);
+    ASSERT(snapshottor);
+    ASSERT(snapshottor->GetType() == CRemoteDesktopSnapshottor::SNAPSHOTTOR_TYPE_X11);
+    CRemoteDesktopSnapshottor_X11 *x11_ss = (CRemoteDesktopSnapshottor_X11*)snapshottor;
 
-	CxDisplay *xdisplay = x11_ss->GetXDisplay();
-	ASSERT(xdisplay);
-	CxWindow *xwindow = x11_ss->GetXWindow();
-	ASSERT(xwindow);
+    int sh = x11_ss->GetSrcHeight();
+    int sw = x11_ss->GetSrcWidth();
+    ASSERT(sw > 0 && sh > 0);
 
-	if(input->GetType() == RD_INPUT_TYPE_MOUSE_INPUT)
-	{
-		CRemoteDesktopMouseInput *mouseInput = input->GetMouseInput();
-		ASSERT(mouseInput);
+    CxDisplay *xdisplay = x11_ss->GetXDisplay();
+    ASSERT(xdisplay);
+    CxWindow *xwindow = x11_ss->GetXWindow();
+    ASSERT(xwindow);
 
-		XTestFakeMotionEvent(
-			xdisplay->GetNativeXDisplay(),
-			-1,
-			mouseInput->GetDx(),
-			mouseInput->GetDy(),
-			CurrentTime
-		);
+    if(input->GetType() == RD_INPUT_TYPE_MOUSE_INPUT)
+    {
+        CRemoteDesktopMouseInput *mouseInput = input->GetMouseInput();
+        ASSERT(mouseInput);
 
-		if(mouseInput->GetDwFlags() & RD_MOUSEEVENTF_LEFTDOWN)
-		{
-			XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),1,1,CurrentTime);
-		}
-		else if(mouseInput->GetDwFlags() & RD_MOUSEEVENTF_LEFTUP)
-		{
-			XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),1,0,CurrentTime);
-		}
-		else if(mouseInput->GetDwFlags() & RD_MOUSEEVENTF_RIGHTDOWN)
-		{
-			XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),3,1,CurrentTime);
-		}
-		else if(mouseInput->GetDwFlags() & RD_MOUSEEVENTF_RIGHTUP)
-		{
-			XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),3,0,CurrentTime);
-		}
-		else if(mouseInput->GetDwFlags() & RD_MOUSEEVENTF_WHEEL)
-		{
-			int id = 4;
-			if(mouseInput->GetMouseData()&0x80000000) //<0
-			{
-				id = 5;
-			}
-			XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),id,1,CurrentTime);
-			XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),id,0,CurrentTime);
-		}
+        XTestFakeMotionEvent(
+            xdisplay->GetNativeXDisplay(),
+            -1,
+            mouseInput->GetDx()*sw/65536,
+            mouseInput->GetDy()*sh/65536,
+            CurrentTime
+        );
 
-		xdisplay->Flush();
-	}
+        if(mouseInput->GetDwFlags() & RD_MOUSEEVENTF_LEFTDOWN)
+        {
+            XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),1,1,CurrentTime);
+        }
+        else if(mouseInput->GetDwFlags() & RD_MOUSEEVENTF_LEFTUP)
+        {
+            XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),1,0,CurrentTime);
+        }
+        else if(mouseInput->GetDwFlags() & RD_MOUSEEVENTF_RIGHTDOWN)
+        {
+            XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),3,1,CurrentTime);
+        }
+        else if(mouseInput->GetDwFlags() & RD_MOUSEEVENTF_RIGHTUP)
+        {
+            XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),3,0,CurrentTime);
+        }
+        else if(mouseInput->GetDwFlags() & RD_MOUSEEVENTF_WHEEL)
+        {
+            int id = 4;
+            if(mouseInput->GetMouseData()&0x80000000) //<0
+            {
+                id = 5;
+            }
+            XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),id,1,CurrentTime);
+            XTestFakeButtonEvent(xdisplay->GetNativeXDisplay(),id,0,CurrentTime);
+        }
 
-	return OK;
+        xdisplay->Flush();
+    }
+
+    return OK;
 }
 //#endif
 
@@ -367,7 +371,7 @@ status_t CRemoteDesktopServer::OnSendInput(CPeerMessage *_message)
     CRetVal_RdSendInput _ret;
     _ret.Init();
 
-	int res = SendLocaInput(this,_param.GetInput());
+    int res = SendLocaInput(this,_param.GetInput());
 
     _ret.SetRes(res);
     this->SendReturnValue(&_context,&_ret);
@@ -378,7 +382,7 @@ status_t CRemoteDesktopServer::OnSendInput(CPeerMessage *_message)
 
 status_t CRemoteDesktopServer::SetRemoteDesktopSnapshottorManager(CRemoteDesktopSnapshottorManager *mgr)
 {
-	m_SnapshottorManager = mgr;
-	return OK;
+    m_SnapshottorManager = mgr;
+    return OK;
 }
 
